d8ca7b65512283a43fed4ad9279255bba36202ab
Joshua Potter <jrpotter2112@gmail.com>
2015-10-01 09:59:26 -0400

    Removal of unnecessary files

--- Project.sim/sim_2/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2014.4\\bin
-call %xv_path%/xsim project_screentest_behav -key {Behavioral:sim_2:Functional:project_screentest} -tclbatch project_screentest.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
